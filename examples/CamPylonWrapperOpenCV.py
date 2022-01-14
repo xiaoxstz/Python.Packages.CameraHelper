e915dde09aa87ffe9ca5732b56471a3a718061b5
Tanzi <None>
2022-01-14 22:18:59 +0800

    complete CamPylonWrapperOpenCV (refer to CamPylonFreerunOpenCV.py)

--- examples/CamPylonWrapperOpenCV.py
+++ examples/CamPylonWrapperOpenCV.py
@@ -1,7 +1,17 @@
 from CameraHelper import CamPylonWrapper
+import cv2
+import numpy as np
 
-cam = CamPylonWrapper()
-ret, img = cam.get_frame()
-if ret:
-    shape = img.shape
-    print(shape)
+if __name__ == '__main__':
+    win_name = "camera"
+    cam = CamPylonWrapper()
+    image= np.zeros([cam.height,cam.height,3],dtype=np.uint8)    
+    while True:
+        ret, image = cam.get_frame()
+        if ret:
+            cv2.imshow(win_name, image)
+            cv2.resizeWindow(win_name,960,960)
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
+    cv2.destroyAllWindows()
