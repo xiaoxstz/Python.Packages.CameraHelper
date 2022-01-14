67586b32c7db8302751f7471387b39e51fd6680c
Tanzi <None>
2022-01-14 22:12:13 +0800

    add opencv demo for CamPylonFreerun

--- examples/CamPylonFreerunOpenCV.py
+++ examples/CamPylonFreerunOpenCV.py
@@ -1,18 +1,32 @@
 from CameraHelper import PylonImageConvert
 from CameraHelper import CamPylonFreerun
 from pypylon import pylon
+import cv2
+import numpy as np
 
 class SampleImageEventHandler(pylon.ImageEventHandler):
     def OnImageGrabbed(self, camera, grabResult):
         """this function should not put the code that costs too much time"""
-        image = PylonImageConvert.convert(grabResult)
-        print("OnImageGrabbed")
+        # print("OnImageGrabbed start")
+        global image
+        img = PylonImageConvert.convert(grabResult)
+        image = img.GetArray()
+        # print("OnImageGrabbed end")
     
     def test(self):
         pass
 
 if __name__ == '__main__':
+    win_name = "camera"
     cam = CamPylonFreerun(SampleImageEventHandler)
-    import time
-    time.sleep(1)
-    cam.Close()
+    image= np.zeros([cam.height,cam.height,3],dtype=np.uint8)    
+    while True:
+        cv2.imshow(win_name, image)
+        cv2.resizeWindow(win_name,960,960)
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
+    cv2.destroyAllWindows()
+
+
+
