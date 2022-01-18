08e290f41906d2b4e5c4a94bbcde1dc8817f2790
Tanzi <None>
2022-01-18 10:39:29 +0800

    add "start_grab_thread" to the module "CamPylonFreerun", so it's usage is the same with other camera libraries

--- src/CameraHelper/CamPylonFreerun.py
+++ src/CameraHelper/CamPylonFreerun.py
@@ -3,25 +3,27 @@ from pypylon import pylon # pip install pypylon
 from pypylon import genicam
 
 class CamPylonFreerun:
-    def __init__(self,imageEvent:pylon.ImageEventHandler) -> None:
+    def __init__(self) -> None:
         try:
             # Create an instant camera object for the camera device found first.
             self.camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
             self.camera.Open()
             self.width = self.camera.Width.GetValue() 
             self.height = self.camera.Height.GetValue()
-
-            # For demonstration purposes only, register another image event handler.
-            self.camera.RegisterImageEventHandler(imageEvent(), pylon.RegistrationMode_Append, pylon.Cleanup_Delete)
-
-            # Start the grabbing using the grab loop thread, by setting the grabLoopType parameter
-            # to GrabLoop_ProvidedByInstantCamera. The grab results are delivered to the image event handlers.
-            # The GrabStrategy_OneByOne default grab strategy is used.
-            self.camera.StartGrabbing(pylon.GrabStrategy_OneByOne, pylon.GrabLoop_ProvidedByInstantCamera)
             
         except genicam.GenericException as e:
             # Error handling.
             print("An exception occurred.", e.GetDescription())
+    
+    def start_grab_thread(self,imageEvent:pylon.ImageEventHandler):
+        # For demonstration purposes only, register another image event handler.
+        self.camera.RegisterImageEventHandler(imageEvent(), pylon.RegistrationMode_Append, pylon.Cleanup_Delete)
+
+        # Start the grabbing using the grab loop thread, by setting the grabLoopType parameter
+        # to GrabLoop_ProvidedByInstantCamera. The grab results are delivered to the image event handlers.
+        # The GrabStrategy_OneByOne default grab strategy is used.
+        self.camera.StartGrabbing(pylon.GrabStrategy_OneByOne, pylon.GrabLoop_ProvidedByInstantCamera)
+        pass
 
     def Close(self):
         if self.camera.IsGrabbing():
@@ -40,7 +42,8 @@ class SampleImageEventHandler(pylon.ImageEventHandler):
         print("OnImageGrabbed")
 
 if __name__ == '__main__':
-    cam = CamPylonFreerun(SampleImageEventHandler)
+    cam = CamPylonFreerun()
+    cam.start_grab_thread(SampleImageEventHandler)
     print(f"image size: ({cam.height},{cam.width})")
     import time
     time.sleep(1)
