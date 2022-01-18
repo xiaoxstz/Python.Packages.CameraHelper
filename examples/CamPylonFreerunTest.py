50996a1805cba078723a399d75ba10be87bb3a5e
Tanzi <None>
2022-01-18 12:02:56 +0800

    CamPylonFreerunTest: add frame_counter

--- examples/CamPylonFreerunTest.py
+++ examples/CamPylonFreerunTest.py
@@ -6,14 +6,19 @@ class SampleImageEventHandler(pylon.ImageEventHandler):
     def OnImageGrabbed(self, camera, grabResult):
         """this function should not put the code that costs too much time"""
         image = PylonImageConvert.convert(grabResult)
-        print("OnImageGrabbed")
+        global frame_counter
+        frame_counter += 1
+        print(f"OnImageGrabbed, frame:{frame_counter}")
     
     def test(self):
         pass
 
 if __name__ == '__main__':
+    frame_counter = 0
     cam = CamPylonFreerun()
     cam.start_grab_thread(SampleImageEventHandler)
     import time
-    time.sleep(1)
+    bExit = False
+    while not bExit:
+        time.sleep(1)
     cam.Close()
