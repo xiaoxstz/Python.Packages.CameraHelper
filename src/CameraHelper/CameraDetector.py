17cf72bcde3a2d20c9b26c62adf036c49a8ca3d9
Tanzi <None>
2022-02-23 09:30:55 +0800

    CameraDetector: add code to detect cameras under the protocol "directshow“

--- src/CameraHelper/CameraDetector.py
+++ src/CameraHelper/CameraDetector.py
@@ -1,17 +1,28 @@
+from pygrabber.dshow_graph import FilterGraph
+
 from pypylon import pylon # pip install pypylon
 from pypylon import genicam
-
-
 class CameraDetector:
     def __init__(self) -> None:
         self.__cam_dict_list = []
     
     def find_cameras(self):
         self.__find_basler_cams()
+        self.__find_directshow_cams()
     
     def get_cams(self):
         return self.__cam_dict_list
-    
+
+    def __find_directshow_cams(self):
+        graph = FilterGraph()
+        devices = graph.get_input_devices()
+        for i in range(len(devices)):
+            cam_dict = dict()
+            cam_dict['index'] = i
+            cam_dict['TL Type'] = "Directshow"
+            cam_dict['Model Name'] = devices[i]
+            self.__cam_dict_list.append(cam_dict)
+
     def __find_basler_cams(self):
         try:
             # Get the transport layer factory.
