8c5239437185be6159a98e70fcf9fe45ed638e39
Tanzi <None>
2022-02-23 14:33:08 +0800

    add CameraDetector to __init__.py

--- src/CameraHelper/__init__.py
+++ src/CameraHelper/__init__.py
@@ -4,3 +4,4 @@ from .CameraType import CameraType
 from .CamPylonWrapper import CamPylonWrapper
 from .CamPylonFreerun import CamPylonFreerun
 from .PylonImageConvert import PylonImageConvert
+from .CameraDetector import CameraDetector
