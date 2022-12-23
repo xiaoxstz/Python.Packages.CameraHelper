35fef4e5025f8fbbfc698a58ef466cebbf89fefb
Tanzi <None>
2022-12-23 12:22:57 +0800

    fix bug: running issues of the sample about common camera

--- examples/CommonCamTest.py
+++ examples/CommonCamTest.py
@@ -1,6 +1,10 @@
 from CameraHelper import CamCommonWrapper
 
-cam = CamCommonWrapper()
+camera_info=dict()
+camera_info['TL Type'] = "Directshow"
+camera_info["index"] = 0
+
+cam = CamCommonWrapper(camera_info)
 if cam.IsConnected():
     ret, img = cam.get_frame()
     if ret:
