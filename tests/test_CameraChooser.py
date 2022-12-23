0d6b63a32f0c5bc44b412cb52cdb932fedf31544
Tanzi <None>
2022-12-23 11:56:30 +0800

    add test cases for "PylonWrapper" and "CommonWrapper"

--- tests/test_CameraChooser.py
+++ tests/test_CameraChooser.py
@@ -32,4 +32,35 @@ class Test(unittest.TestCase):
                 print("failed to open the camera")
         else:
             print("found no basler camera")
+    
+    def test_PylonWrapper(self):
+        camera_info_list= CameraDetector.find_basler_cams()
+        if len(camera_info_list) > 0:
+            camera_info = camera_info_list[0]
+            
+            bSucceed, camera = CameraChooser.Choose(CameraType.PylonWrapper,camera_info)
+            if camera.IsConnected():
+                camera.start_grab_thread(SampleImageEventHandler)
+                import time
+                bExit = False
+                while not bExit:
+                    time.sleep(1)
+                camera.Close()
+            else:
+                print("failed to open the camera")
+        else:
+            print("found no basler camera")
+    
+    def test_CommonWrapper(self):
+        camera_info = dict()
+        bSucceed, camera = CameraChooser.Choose(CameraType.CommonWrapper,camera_info)
+        if bSucceed and camera.IsConnected():
+            camera.start_grab_thread(SampleImageEventHandler)
+            import time
+            bExit = False
+            while not bExit:
+                time.sleep(1)
+            camera.Close()
+        else:
+            print("failed to open the camera")
 
