339a0b5bf19dc8b0921bcf55a95bee2dab60e780
Tanzi <None>
2022-12-24 17:44:11 +0800

    find the files automatically with the patterns

--- src/setup.py
+++ src/setup.py
@@ -1,24 +1,20 @@
 from setuptools import setup
 from Cython.Build import cythonize
 
+import glob # find path with given pattern
+
 import sys,os
 cur_path = sys.path[0]
 os.chdir(cur_path)
 
-para = sys.argv[1]
-module_list = [
-    "./CameraHelper/CamCommonWrapper.py",
-    "./CameraHelper/CamPylonFreerun.py",
-    "./CameraHelper/CamPylonWrapper.py",
-    "./CameraHelper/PylonImageConvert.py",
-    "./CameraHelper/CameraDetector.py",
-    "./CameraHelper/CameraType.py",
-    "./CameraHelper/CameraChooser.py",
-    "./CameraHelper/__init__.py",
-]
+# para = sys.argv[1]
+file_patterns =["./CameraHelper/*.py",]
+file_list = []
+for pattern in file_patterns:
+    file_list += glob.glob(pattern)
 
 setup(
     name="CameraHelper",
-    ext_modules=cythonize(module_list),
+    ext_modules=cythonize(file_list),
     zip_safe=False,
 )
